8502ed5b254209264c1d155466cc9609c912fbeb
Grasshoqper <133687093+Grasshoqper@users.noreply.github.com>
2024-03-07 19:49:30 -0500

    Create pivotEncoder.java

---
+++ pivotEncoder.java
@@ -0,0 +1,159 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.TimedRobot;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+
+public class Robot extends TimedRobot {
+  // motors
+  CANSparkMax intake = new CANSparkMax(4, MotorType.kBrushless);
+  CANSparkMax flywheelLeft = new CANSparkMax(10, MotorType.kBrushless);
+  CANSparkMax driveLeftA = new CANSparkMax(2, MotorType.kBrushed);
+  CANSparkMax driveLeftB = new CANSparkMax(1, MotorType.kBrushed);
+  CANSparkMax flywheelRight = new CANSparkMax(6, MotorType.kBrushless);
+  CANSparkMax intakePivot = new CANSparkMax(7, MotorType.kBrushless);
+  CANSparkMax driveRightA = new CANSparkMax(9, MotorType.kBrushed);
+  CANSparkMax driveRightB = new CANSparkMax(5, MotorType.kBrushed);
+  
+  // controllers
+  XboxController driveControllerA = new XboxController(0);
+  XboxController driveControllerB = new XboxController(1);
+
+  // encoders
+  private RelativeEncoder intakePivotEncoder;
+
+
+
+  // drive
+
+
+
+  @Override 
+  public void robotInit() {
+  // directionals
+  intake.setInverted(false);
+  flywheelLeft.setInverted(true);
+  driveLeftA.setInverted(true);
+  flywheelRight.setInverted(false);
+  intakePivot.setInverted(false);
+  driveRightA.setInverted(false);
+
+
+  // set all speeds 0
+  intake.set(0);
+  flywheelLeft.set(0);
+
+  driveLeftA.set(0);
+  driveLeftB.set(0);
+  flywheelRight.set(0);
+  //intakePivot.set(0);
+
+  driveRightA.set(0);
+  driveRightB.set(0);
+  
+  // encoders
+  intakePivotEncoder = intakePivot.getEncoder();
+  
+
+  // setting slave motors
+  driveLeftB.follow(driveLeftA);
+  driveRightB.follow(driveRightA);
+
+  // drive
+
+
+
+  
+  // auto 
+
+
+
+
+  }
+  @Override
+  public void robotPeriodic() {
+   
+
+
+  }
+
+ 
+
+  @Override
+  public void autonomousInit() {
+    
+
+  }
+ 
+
+  @Override
+  public void teleopInit() {
+
+  }
+  final double kP = 0.05;
+
+  double setpoint = 0;
+
+
+  private double revTime = 0.0;
+  private double endShoot = 0.0;
+
+  private boolean buttonPressed = false;
+
+  private boolean intakeActive = false;
+  private boolean outakeActive = false;
+
+  
+  @Override
+  public void teleopPeriodic() {
+    // encoders
+    SmartDashboard.putNumber("Encoder Position", intakePivotEncoder.getPosition());
+
+    intakePivot.set(driveControllerB.getLeftY() * 0.5);
+    // drive
+    
+  
+  // flywheels/speaker
+    
+    // speaker
+
+
+
+   
+  }
+
+  @Override
+  public void disabledInit() {
+  // set all speeds 0
+  intake.set(0);
+  flywheelLeft.set(0);
+
+  driveLeftA.set(0);
+  driveLeftB.set(0);
+  flywheelRight.set(0);
+  //intakePivot.set(0);
+
+  driveRightA.set(0);
+  driveRightB.set(0);
+  }
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+}
